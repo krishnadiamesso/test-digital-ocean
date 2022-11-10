68ba2705a15973e74b318d6c9fcdbc96e7a096fa
Krishna Diamesso <krishnadiamesso@gmail.com>
2022-11-10 14:10:12 -0500

    Add logger

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module TestDigitalOcean
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    Rails.logger = Logger.new(STDOUT)
   end
 end
