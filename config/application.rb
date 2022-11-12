1b4e45416ecc65b98f8803e5a9c95e5f6dcf791e
Krishna Diamesso <krishnadiamesso@gmail.com>
2022-11-12 00:02:10 -0500

    Remove logger

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module TestDigitalOcean
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    Rails.logger = Logger.new(STDOUT)
+    # Rails.logger = Logger.new(STDOUT)
   end
 end
